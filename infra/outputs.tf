c056ae41f1ae6f9fc0f0f26d58937a4886275d1c
Yat Yee Li <twistedogic@gmail.com>
2023-07-16 16:09:28 +0800

    chore(infra): add security group for API access

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.take-me-home.public_ip
 }
+
+output "instance_public_url" {
+  description = "Public URL of the EC2 instance"
+  value       = aws_instance.take-me-home.public_dns
+}
